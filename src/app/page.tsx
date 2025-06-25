0dbca6e18361119944345777e1ce47681132c4a4
Susan Harrison <8228harrisons@gmail.com>
2025-06-24 22:11:38 -0400

    minor update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <h1 className="text-4xl font-bold">WiredByWeather</h1>
       <p className="text-lg">
         Welcome to Wired by Weather: advancing science, education, and global unity.
-        <br />Website development in progress. 6/20/2025.
+        <br />Website development in progress. Last update: 6/24/2025.
         <br />Stay tuned.
       </p>
 
@@ -42,7 +42,7 @@ export default function Home() {
         <label htmlFor="comment">Share your vision for peace & science:</label>
         <textarea
           id="comment"
-          className="w-full p-2 rounded border"
+          className="b-full p-2 rounded border"
           rows={4}
           value={comment}
           onChange={(e) => setComment(e.target.value)}
